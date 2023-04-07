9aa5896d0b6fddefe4e70ce1177ad9f662c7fd5c
regularjoe <53127823+benjamsf@users.noreply.github.com>
2023-04-07 21:24:27 +0300

    Add in progress bar as it takes time

--- src/text_preparation/tokenize_latin.py
+++ src/text_preparation/tokenize_latin.py
@@ -1,33 +1,42 @@
 # Python script to prepare Luther's Werke im WWW text to form usable in NLP
 # Stage 2, tokenize prepared text
 # (c) Benjam Bröijer, licensed under the MIT License
-import os
-from cltk import NLP
-from cltk.tokenizers.word import WordTokenizer
 
+import os
+import string
+from cltk import NLP
+from tqdm import tqdm
 
 # Instantiate a Latin-specific NLP object
 cltk_nlp = NLP(language="lat")
 
-
-
 # Define the source and destination file names and paths
-# Target text to projectfolder/txt/target_txt
-# Output text to projectfolder/txt/output_txt
 script_path = os.path.dirname(os.path.abspath(__file__))
-source_path = os.path.join(script_path, '../../output/dsa_prepared_1.txt')
-destination_path = os.path.join(script_path, '../../output/dsa_tokenized.pkl')
+source_path = os.path.join(script_path, '../../output/dsa_prepared_2.txt')
+destination_path = os.path.join(script_path, '../../output/dsa_tokenized2.pkl')
 
 # Load the Latin text from the source file
 with open(source_path, 'r', encoding='utf-8') as f:
     input_text = f.read()
 
-doc = cltk_nlp(input_text)
-word_tokens = [word.string for word in doc.words]
+# Remove punctuation marks from the input text
+translator = str.maketrans("", "", string.punctuation)
+input_text_no_punctuation = input_text.translate(translator)
+
+# Split the input text into smaller chunks
+text_chunks = input_text_no_punctuation.split()
+
+# Process the text_chunks with cltk_nlp and update the progress bar
+word_tokens = []
+for chunk in tqdm(text_chunks, desc="Tokenizing words"):
+    doc = cltk_nlp(chunk)
+    for word in doc.words:
+        word_tokens.append(word.string)
+
 print(word_tokens)
 
 # Save the tokenized output to a file
 with open(destination_path, 'w', encoding='utf-8') as f:
-    f.write(' '.join(output_doc.tokens))
+    f.write(' '.join(word_tokens))
 # Print a message to confirm that the file has been saved
 print(f'The tokenized output has been saved as {destination_path}')
--- src/text_preparation/tokenize_latin_old.py
+++ src/text_preparation/tokenize_latin_old.py
@@ -1,20 +1,16 @@
 # Python script to prepare Luther's Werke im WWW text to form usable in NLP
-# Stage 2, tokenize prepared text
+# Stage 2a, tokenize prepared text by words
 # (c) Benjam Bröijer, licensed under the MIT License
 
 import os
+import time
 from cltk import NLP
-from cltk.tokenizers.word import WordTokenizer
-
+from tqdm import tqdm
 
 # Instantiate a Latin-specific NLP object
 cltk_nlp = NLP(language="lat")
 
-
-
 # Define the source and destination file names and paths
-# Target text to projectfolder/txt/target_txt
-# Output text to projectfolder/txt/output_txt
 script_path = os.path.dirname(os.path.abspath(__file__))
 source_path = os.path.join(script_path, '../../output/dsa_prepared_2.txt')
 destination_path = os.path.join(script_path, '../../output/dsa_tokenized2.pkl')
@@ -23,8 +19,28 @@ destination_path = os.path.join(script_path, '../../output/dsa_tokenized2.pkl')
 with open(source_path, 'r', encoding='utf-8') as f:
     input_text = f.read()
 
+# Measure the start time
+start_time = time.time()
+
+# Tokenize the input_text using cltk_nlp
 doc = cltk_nlp(input_text)
-word_tokens = [word.string for word in doc.words]
+
+# Measure the end time
+end_time = time.time()
+
+# Calculate the time taken for tokenization
+time_taken = end_time - start_time
+
+# Calculate the number of tokens processed per second
+tokens_per_second = len(doc.words) / time_taken
+print(f"Tokens processed per second: {tokens_per_second}")
+
+word_tokens = []
+
+# Add a progress bar using tqdm
+for i, word in enumerate(tqdm(doc.words, desc="Tokenizing words")):
+    word_tokens.append(word.string)
+
 print(word_tokens)
 
 # Save the tokenized output to a file
