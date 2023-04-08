964e5ae86554acf1e2a3fe9333e83e7371df821d
regularjoe <53127823+benjamsf@users.noreply.github.com>
2023-04-09 01:12:17 +0300

    Delete runtest.bat

--- runtest.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set PYTHONIOENCODING=utf-8
-set "PYTHON_SCRIPTS_PATH=C:\Users\bg1\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\Scripts"
-set "PATH=%PYTHON_SCRIPTS_PATH%;%PATH%"
-lutherscripts-cli -o word_tokenize_latin -s 'G:\\Kirjotelmat\\2018\\HY\\I_IV_Proseminaari\\Ver 3\\GRADU\\Uusiversio\\Teksti\\python-deservoarbitrio-textanalysis\\lutherscripts\\txt\\dsatest.txt' -d 'G:\\Kirjotelmat\\2018\\HY\\I_IV_Proseminaari\\Ver 3\\GRADU\\Uusiversio\\Teksti\\python-deservoarbitrio-textanalysis\\lutherscripts\\txt\\teset.txt'
