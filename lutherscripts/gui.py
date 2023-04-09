d0da526e9ce1cebc8a232f3badaa60f146665213
Benjam <53127823+benjamsf@users.noreply.github.com>
2023-04-09 15:27:45 +0300

    add implementation of KWIC analysis, not tested yet

--- lutherscripts/gui.py
+++ lutherscripts/gui.py
@@ -19,7 +19,7 @@ def create_image_label(parent):
     luther_photo = tk.PhotoImage(data=image_bytes)
     lbl_luther_image = tk.Label(parent, image=luther_photo)
     lbl_luther_image.image = luther_photo
-    lbl_luther_image.pack(side=tk.LEFT, padx=10, pady=10)
+    lbl_luther_image.grid(row=0, rowspan=5, column=0, padx=10, pady=10)
     return lbl_luther_image
 
 
@@ -29,27 +29,60 @@ def gui_main():
     root.geometry("1400x600")
     root.title("LutherScripts v0.2.0 - A NLP toolset for Latin language")
 
-    # create the tabs
-    tab_parent = ttk.Notebook(root)
-    tab1 = ttk.Frame(tab_parent)
-    tab2 = ttk.Frame(tab_parent)
-    tab3 = ttk.Frame(tab_parent)
-    tab_parent.add(tab1, text="Config")
-    tab_parent.add(tab2, text="Text Preparation")
-    tab_parent.add(tab3, text="Text Processing")
-    tab_parent.pack(expand=1, fill='both')
+    # create widgets for Operations tab
+    create_image_label(root)
 
-    # create widgets for Config tab
-    create_image_label(tab1)
-    lbl_raw_sourcetext = tk.Label(tab1, text="Choose raw source text:")
-    lbl_raw_sourcetext.pack(side=tk.LEFT, padx=10, pady=10)
-    btn_raw_sourcetext = tk.Button(tab1, text="Browse...", command=lambda: choose_file())
-    btn_raw_sourcetext.pack(side=tk.LEFT, padx=10, pady=10)
+    # Choose raw source text
+    lbl_raw_sourcetext = tk.Label(root, text="Choose raw source text:")
+    lbl_raw_sourcetext.grid(row=0, column=1, padx=10, pady=10)
+    btn_raw_sourcetext = tk.Button(root, text="Browse...", command=lambda: choose_file())
+    btn_raw_sourcetext.grid(row=0, column=2, padx=10, pady=10)
 
     file_label = tk.StringVar()
     file_label.set("No file selected")
-    lbl_selected_file = tk.Label(tab1, textvariable=file_label)
-    lbl_selected_file.pack(side=tk.LEFT, padx=10, pady=10)
+    lbl_selected_file = tk.Label(root, textvariable=file_label)
+    lbl_selected_file.grid(row=0, column=3, padx=10, pady=10)
+
+    # Choose output file location
+    lbl_output_file = tk.Label(root, text="Choose output file location:")
+    lbl_output_file.grid(row=1, column=1, padx=10, pady=10)
+    btn_output_file = tk.Button(root, text="Browse...", command=lambda: choose_output_file())
+    btn_output_file.grid(row=1, column=2, padx=10, pady=10)
+
+    output_file_label = tk.StringVar()
+    output_file_label.set("No file selected")
+    lbl_selected_output_file = tk.Label(root, textvariable=output_file_label)
+    lbl_selected_output_file.grid(row=1, column=3, padx=10, pady=10)
+
+    # Choose Operation
+    lbl_operation = tk.Label(root, text="Choose Operation:")
+    lbl_operation.grid(row=2, column=1, padx=10, pady=10)
+
+    options = [
+        ("word_tokenize_latin", "Tokenize Latin text by words"),
+        ("sent_tokenize_latin", "Tokenize Latin text by sentences"),
+        ("nltk_do_kwic", "Perform KWIC analysis"),
+    ]
+
+    var_operation = tk.StringVar(root)
+    var_operation.set(options[0][1])
+    opt_operation = tk.OptionMenu(root, var_operation, *[option[1] for option in options])
+    opt_operation.grid(row=2, column=2, padx=10, pady=10)
+
+    explanation_label = tk.StringVar()
+    explanation_label.set("This operation will tokenize your Latin text by words, which is required for further word-based natural language processing.")
+    lbl_explanation = tk.Label(root, textvariable=explanation_label, wraplength=300)
+    lbl_explanation.grid(row=2, column=3, padx=10, pady=10)
+
+    # Start Operation! button
+    btn_play = tk.Button(root, text="Start Operation!", command=run_script)
+    btn_play.grid(row=3, column=3, padx=10, pady=10)
+
+    # Terminal output
+    txt_terminal = tk.Text(root, height=20, width=1000)
+    txt_terminal.grid(row=4, column=1, columnspan=3, padx=10, pady=10, sticky='nsew')
+    root.grid_columnconfigure(1, weight=1)
+    root.grid_rowconfigure(4, weight=1)
 
     # function to choose file and store the location in a variable
     def choose_file():
@@ -58,41 +91,12 @@ def gui_main():
         file_label.set(location_raw_sourcetext)
         print(f"File selected: {location_raw_sourcetext}")
 
-    lbl_output_file = tk.Label(tab1, text="Choose output file location:")
-    lbl_output_file.pack(side=tk.LEFT, padx=10, pady=10)
-    btn_output_file = tk.Button(tab1, text="Browse...", command=lambda: choose_output_file())
-    btn_output_file.pack(side=tk.LEFT, padx=10, pady=10)
-
-    output_file_label = tk.StringVar()
-    output_file_label.set("No file selected")
-    lbl_selected_output_file = tk.Label(tab1, textvariable=output_file_label)
-    lbl_selected_output_file.pack(side=tk.LEFT, padx=10, pady=10)
-
     # function to choose output file and store the location in a variable
     def choose_output_file():
         global location_output
         location_output = filedialog.asksaveasfilename(title="Select output file location", defaultextension=".txt", initialdir=os.path.dirname(os.path.abspath(__file__)))
         output_file_label.set(location_output)
         print(f"Output file selected: {location_output}")
-
-
-    # create widgets for Text Preparation tab
-    create_image_label(tab2)
-    lbl_operation = tk.Label(tab2, text="Choose Operation:")
-    lbl_operation.pack(side=tk.LEFT, padx=10, pady=10)
-    options = [
-        ("word_tokenize_latin", "Tokenize Latin text by words"),
-        ("sent_tokenize_latin", "Tokenize Latin text by sentences"),
-    ]
-    var_operation = tk.StringVar(tab2)
-    var_operation.set(options[0][1])
-    opt_operation = tk.OptionMenu(tab2, var_operation, *[option[1] for option in options])
-    opt_operation.pack(side=tk.LEFT, padx=10, pady=10)
-    
-    explanation_label = tk.StringVar()
-    explanation_label.set("This operation will tokenize your Latin text by words, which is required for further word-based natural language processing.")
-    lbl_explanation = tk.Label(tab2, textvariable=explanation_label, wraplength=300)
-    lbl_explanation.pack(side=tk.LEFT, padx=10, pady=10)
 
     def run_script():
         global location_raw_sourcetext, location_output
@@ -110,37 +114,9 @@ def gui_main():
             txt_terminal.see(tk.END)
         returncode = process.poll()
 
-
-    btn_play = tk.Button(tab2, text="Play!", command=run_script)
-    btn_play.pack(side=tk.LEFT, padx=10, pady=10)
-
-    txt_terminal = tk.Text(tab2, height=20, width=1000)
-    txt_terminal.pack(side=tk.BOTTOM, padx=10, pady=10, fill=tk.BOTH, expand=True)
-    tab2.grid_columnconfigure(0, weight=1)
-
-    # create widgets for Text Processing tab
-    lbl_nothing = tk.Label(tab3, text="Choose Operation:")
-    lbl_nothing.pack(side=tk.LEFT, padx=10, pady=10)
-
-    processing_options = {
-        "nltk_do_kwic": "Perform KWIC analysis"
-    }
-    var_processing_operation = tk.StringVar(tab3)
-    var_processing_operation.set(next(iter(processing_options.keys())))
-    opt_processing_operation = tk.OptionMenu(tab3, var_processing_operation, *processing_options.keys())
-    opt_processing_operation.pack(side=tk.LEFT, padx=10, pady=10)
-
-    lbl_processing_explanation = tk.Label(tab3, text=processing_options[var_processing_operation.get()], wraplength=250, justify=tk.LEFT)
-    lbl_processing_explanation.pack(side=tk.LEFT, padx=10, pady=10)
-
-    def on_processing_option_change(*args):
-        operation_key = var_processing_operation.get()
-        lbl_processing_explanation.config(text=processing_options[operation_key])
-
-    var_processing_operation.trace('w', on_processing_option_change)
-
-# start the GUI
+    # start the GUI
     root.mainloop()
 
 if __name__ == '__main__':
     gui_main()
+
--- lutherscripts/src/text_processing/nltk_do_kwic.py
+++ lutherscripts/src/text_processing/nltk_do_kwic.py
@@ -7,34 +7,31 @@ from nltk.text import Text
 __author__ = "benjamsf"
 __license__ = "MIT"
 
-# Define the source and destination file names and paths
-script_path = os.path.dirname(os.path.abspath(__file__))
-source_path = os.path.join(script_path, '../../output/dsa_tokenized.pkl')
-destination_path = os.path.join(script_path, '../../output/dsa_kwic_analysis.json')
 
-# Load the tokenized text from the source file
-with open(source_path, 'r', encoding='utf-8') as f:
-    tokenized_text = f.read()
+def main(keyword, context_size, source_path, destination_path):
+    # Load the tokenized text from the source file
+    with open(source_path, 'r', encoding='utf-8') as f:
+        tokenized_text = f.read()
 
-# Create an NLTK Text object
-text = Text(tokenized_text)
+    # Create an NLTK Text object
+    text = Text(nltk.word_tokenize(tokenized_text))
 
-# Perform KWIC analysis for the keyword
-keyword = "spe"
-kwic_results = list(text.concordance_list(keyword, lines=nltk.ConcordanceIndex(text.tokens).count(keyword)))
+    # Perform KWIC analysis for the keyword
+    kwic_results = list(text.concordance_list(keyword, lines=nltk.ConcordanceIndex(text.tokens).count(keyword), width=context_size))
 
-# Convert the KWIC results into JSON format
-kwic_json = []
-for result in kwic_results:
-    kwic_json.append({
-        "left_context": " ".join(result[0]),
-        "keyword": result[1],
-        "right_context": " ".join(result[2])
-    })
+    # Convert the KWIC results into JSON format
+    kwic_json = []
+    for result in kwic_results:
+        kwic_json.append({
+            "left_context": " ".join(result[0]),
+            "keyword": result[1],
+            "right_context": " ".join(result[2])
+        })
 
-# Save the KWIC analysis as a JSON file
-with open(destination_path, 'w', encoding='utf-8') as f:
-    json.dump(kwic_json, f, ensure_ascii=False, indent=2)
+    # Save the KWIC analysis as a JSON file
+    with open(destination_path, 'w', encoding='utf-8') as f:
+        json.dump(kwic_json, f, ensure_ascii=False, indent=2)
 
-# Print a message to confirm that the file has been saved
-print(f'The KWIC analysis has been saved as {destination_path}')
+    # Print a message to confirm that the file has been saved
+    print(f'The KWIC analysis has been saved as {destination_path}')
+
