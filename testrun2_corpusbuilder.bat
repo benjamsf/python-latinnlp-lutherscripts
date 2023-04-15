1ce333c3b414aad5412ae7c0f28eecd606b2dfd4
regularjoe <53127823+benjamsf@users.noreply.github.com>
2023-04-15 20:56:54 +0300

    add extra latin stopwords

--- testrun2_corpusbuilder.bat
+++ testrun2_corpusbuilder.bat
@@ -2,4 +2,4 @@
 set PYTHONIOENCODING=utf-8
 set "PYTHON_SCRIPTS_PATH=C:\Users\bg1\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\Scripts"
 set "PATH=%PYTHON_SCRIPTS_PATH%;%PATH%"
-lutherscripts-cli -o build_corpus -s lutherscripts/txt/jalla.json -d lutherscripts/output/DSAkoe
+lutherscripts-cli -o build_corpus -s lutherscripts/output/dsa_tokenized.json -d lutherscripts/output/DSACorpus1
--- testrun3_topicmodeler.bat
+++ testrun3_topicmodeler.bat
@@ -2,4 +2,4 @@
 set PYTHONIOENCODING=utf-8
 set "PYTHON_SCRIPTS_PATH=C:\Users\bg1\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\Scripts"
 set "PATH=%PYTHON_SCRIPTS_PATH%;%PATH%"
-lutherscripts-cli -o topic_modeling -1 3 -2 30 -3 100 -s lutherscripts/txt/jalla.json -c lutherscripts/output/DSAkoe_corpus.mm -dc lutherscripts/output/DSAkoe_dictionary.pkl -d lutherscripts/output/DSA_topictest.json
+lutherscripts-cli -o topic_modeling -1 3 -2 100 -3 50 -s lutherscripts/output/dsa_tokenized.json -c lutherscripts/output/DSACorpus1_corpus.mm -dc lutherscripts/output/DSACorpus1_dictionary.pkl -d lutherscripts/output/DSACorpus1_topictest.json
